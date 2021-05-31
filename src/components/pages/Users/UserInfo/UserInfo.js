aafb46e7162abe311f276173a9876c56237a0c9d
IhorKurylov <idSNrY7I3xhB>
2021-05-31 21:06:37 +0300

    users

--- src/components/pages/Users/UserInfo/UserInfo.js
+++ src/components/pages/Users/UserInfo/UserInfo.js
@@ -3,10 +3,15 @@ import React from "react";
 
 import classes from './UserInfo.module.css'
 
-const UserInfo = () =>{
+const UserInfo = ({user}) =>{
+    console.log(user)
  return(
      <ul className={classes.UserInfo}>
-       
+       <p>Name: {user.name}</p>
+       <p>Username: {user.username}</p>
+       <p>Email: {user.email}</p>
+       <p>Company:</p>
+       <p>Company name: {user.company.name}</p>
      </ul>
  )
 }
